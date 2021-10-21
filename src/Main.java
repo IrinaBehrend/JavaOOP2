412cecb6cf00446b914e0c27c8bddb440c9612d5
Irina Behrend <irinabnd@gmail.com>
2021-10-21 00:40:51 -0500

    one update

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         Worker worker = new Worker(10000, "Dina");
-        Manager manager = new Manager(12000, "Tina", 2);
+        Manager manager = new Manager(12000, "Victor", 2);
         Director director = new Director(12000, "Lina", 3);
 
         Worker[] workers = {worker, manager, director};
         Manager[] managers = {manager, director};
 
         System.out.println(EmployeeUtils.findByName("Irina", workers).getName());
-        System.out.println(EmployeeUtils.findBySubName("in", workers).getName());
+        System.out.println(EmployeeUtils.findBySubName("ct", workers).getName());
         System.out.println(EmployeeUtils.getMinSubordinates(managers).getName());
     }
 }
