dd828336c5299fea6a4cb462ee8577009eafe6cc
Irina Behrend <irinabnd@gmail.com>
2021-10-21 23:36:27 -0500

    some code changes

---
+++ src/IName.java
@@ -0,0 +1,5 @@
+public interface IName {
+
+    public String getName();
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
 public class Main {
 
+    public static void printName(IName[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i].getName());
+        }
+    }
+
     public static void main(String[] args) {
 
         Worker worker = new Worker(10000, "Dina");
         Manager manager = new Manager(12000, "Victor", 2);
-        Director director = new Director(12000, "Lina", 3);
+//        Director director = new Director(12000, "Lina", 3);
 
-        Worker[] workers = {worker, manager, director};
-        Manager[] managers = {manager, director};
+//        Worker[] workers = {worker, manager, director};
+//        Manager[] managers = {manager, director};
 
-        System.out.println(EmployeeUtils.findByName("Irina", workers).getName());
-        System.out.println(EmployeeUtils.findBySubName("ct", workers).getName());
-        System.out.println(EmployeeUtils.getMinSubordinates(managers).getName());
+        Employee[] arr = {worker, manager};
+
+        IName[] arr2 = {worker, MonthHelper.MONTHS[0]};
+
+        printName(arr2);
+
+
+//        System.out.println(EmployeeUtils.findByName("Irina", workers).getName());
+//        System.out.println(EmployeeUtils.findBySubName("ct", workers).getName());
+//        System.out.println(EmployeeUtils.getMinSubordinates(managers).getName());
     }
 }
